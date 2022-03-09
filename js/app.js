20c9ae8ab3feca0b4b0d4b611cdfca30bddc8a95
ElisaSoco <esocolsky@gmail.com>
2022-03-08 21:47:00 -0300

    ejercicio completo corregido

--- js/app.js
+++ js/app.js
@@ -21,47 +21,77 @@ let tweets = [];
 cargarListeners();
 function cargarListeners(){
     tweetForm.addEventListener('submit', agregarTweet);
-    tweets.addEventListener('click', eliminarTweet);
-
-
-
+    alltweets.addEventListener('click', eliminarTweet);
+    document.addEventListener('DOMContentLoaded', ()=>{
+        tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+        tweetsHTML();
+    })
 };
 function agregarTweet(e){
     e.preventDefault();
     if(tweetElement.value === '') {
-      alert('tweet vacio');
-      return;
+      const paragraph = document.createElement('p');
+      const errorMessage = 'no se puede ingresar un calor vacío';
+      paragraph.textContent = errorMessage;
+      paragraph.style.color = 'red';
+      paragraph.classList.add('error');
+      tweetForm.append(paragraph);
+      return;// el return lo usamos para que no se siga ejecutando el codigo en caso de que estemos en el error
     }else if ((tweetElement.value).length > 140){
-        alert('tweet con mas de 140 caracteres');
-        return;
+        const paragraph = document.createElement('p');
+      const errorMessage = 'no se puede ingresar un texto que supere los 140 caracteres';
+      paragraph.textContent = errorMessage;
+      paragraph.style.color = 'red';
+      paragraph.classList.add('error');
+      tweetForm.append(paragraph);
+      return;
+    };
+    if(tweetForm.lastElementChild.classList.contains('error')){
+        tweetForm.removeChild(tweetForm.lastElementChild);//-->este se usa para borrar el mensaje de error una vez que nos tiro el error y ponemos de manera correcta desaparece
     }
-    tweets.push(tweetElement.value);
+    const tweet = {
+        content: tweetElement.value,
+        id: Date.now().toString()
+    }
+    tweets.push(tweet);
     tweetsHTML();
     tweetForm.reset();
   };
 
 function tweetsHTML(){
-    //vaciarCarrito();
+    limpiarHTML();
     tweets.forEach(tweet=>{
         const parrafo = document.createElement('p');
         parrafo.innerHTML= `
         <p>
-            ${tweet}
+            ${tweet.content}
         </p>
         <button>
-            <a href = "#" class="borrar-tweet" data-id="">X</a>
+        <a href = "#" class="borrar-tweet" data-id="${tweet.id}">X</a>
         </button>
         `;
-        //sincronizarStorage();
+        sincronizarStorage();
         allTweets.append(parrafo)
     });
 };
 
-// function eliminarTweet(e){
-//     e.preventDefault()
-//     if(e.target.classList.contains('borrar-tweet')){
-//         const cursoId = e.target.getAttribute('data-id');
-//         tweets = tweets.filter(tweet => tweet.id !== tweetId);
-//         tweetsHTML();
-//     }
-// }
+function sincronizarStorage(){
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+};
+
+function limpiarHTML(){
+    while(allTweets.firstChild){
+        allTweets.removeChild(allTweets.firstChild);
+    }
+    //se usa para limpiar el HTML para que los tweets no aparezcan duplicados, si no estariamos sobreiterando
+
+}
+
+function eliminarTweet(e){
+    e.preventDefault()
+    if(e.target.classList.contains('borrar-tweet')){
+        const tweetId = e.target.getAttribute('data-id');
+        tweets = tweets.filter(tweet => tweet.id !== tweetId);
+        tweetsHTML();
+    }
+}
